fb44ce71eb4fc7d4327ef660b77dd7c1a70deafa
Erazem Kokot <mail@erazem.eu>
2022-05-10 17:37:18 +0200

    Add splash activity

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp3/ui/SplashActivity.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp3/ui/SplashActivity.kt
@@ -1,6 +1,26 @@
 package si.uni_lj.fri.pbd.miniapp3.ui
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import si.uni_lj.fri.pbd.miniapp3.databinding.ActivitySplashBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
 }
