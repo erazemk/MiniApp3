18e745b9aad571ce108e2c10a76a49838dde4d41
Erazem Kokot <mail@erazem.eu>
2022-05-10 21:34:16 +0200

    Fix RestAPI.kt

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp3/rest/RestAPI.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp3/rest/RestAPI.kt
@@ -2,19 +2,21 @@ package si.uni_lj.fri.pbd.miniapp3.rest
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import si.uni_lj.fri.pbd.miniapp3.models.dto.IngredientsDTO
 import si.uni_lj.fri.pbd.miniapp3.models.dto.RecipesByIdDTO
 import si.uni_lj.fri.pbd.miniapp3.models.dto.RecipesByIngredientDTO
 
+// Source: https://www.vogella.com/tutorials/Retrofit/article.html
 interface RestAPI {
 
     @get:GET("list.php?i=list")
     val allIngredients: Call<IngredientsDTO?>?
 
-    @get:GET("filter.php?i={ingredient}")
-    val mainIngredient: Call<RecipesByIngredientDTO?>?
+    @GET("filter.php?i={ingredient}")
+    fun getByIngredient(@Path("ingredient") ingredient: String?): Call<RecipesByIngredientDTO?>?
 
-    @get:GET("lookup.php?i={id}")
-    val recipeById: Call<RecipesByIdDTO?>?
+    @GET("lookup.php?i={id}")
+    fun getRecipeById(@Path("id") id: String?): Call<RecipesByIdDTO?>?
 
 }
