837009cb28e072470a4d2e4cdea1c59d525fd013
Erazem Kokot <mail@erazem.eu>
2022-05-10 18:24:45 +0200

    Implement Database.kt

---
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp3/database/Database.kt
@@ -0,0 +1,34 @@
+package si.uni_lj.fri.pbd.miniapp3.database
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import si.uni_lj.fri.pbd.miniapp3.Constants
+import si.uni_lj.fri.pbd.miniapp3.database.dao.RecipeDao
+import si.uni_lj.fri.pbd.miniapp3.database.entity.RecipeDetails
+
+@androidx.room.Database(entities = [RecipeDetails::class], version = 1, exportSchema = false)
+abstract class Database : RoomDatabase() {
+
+    abstract fun recipeDao() : RecipeDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: Database? = null
+
+        fun getDatabase(context: Context): Database? {
+            if (INSTANCE == null) {
+                synchronized(Database::class.java) {
+                    if (INSTANCE == null) {
+                        INSTANCE = Room.databaseBuilder(context.applicationContext,
+                            Database::class.java, Constants.DB_NAME)
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    }
+                }
+            }
+
+            return INSTANCE
+        }
+    }
+}
