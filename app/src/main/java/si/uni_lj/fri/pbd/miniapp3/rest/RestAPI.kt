23367520910ba564c3fbfc6323ff1d804e2059e7
Erazem Kokot <mail@erazem.eu>
2022-05-10 18:10:14 +0200

    Define API endpoints

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp3/rest/RestAPI.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp3/rest/RestAPI.kt
@@ -1,17 +1,20 @@
 package si.uni_lj.fri.pbd.miniapp3.rest
 
-import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.Path
-import retrofit2.http.Query
-import si.uni_lj.fri.pbd.miniapp3.models.dto.*
+import si.uni_lj.fri.pbd.miniapp3.models.dto.IngredientsDTO
+import si.uni_lj.fri.pbd.miniapp3.models.dto.RecipesByIdDTO
+import si.uni_lj.fri.pbd.miniapp3.models.dto.RecipesByIngredientDTO
 
 interface RestAPI {
 
     @get:GET("list.php?i=list")
     val allIngredients: Call<IngredientsDTO?>?
 
-    // TODO: Add missing endpoints
+    @get:GET("filter.php?i={ingredient}")
+    val mainIngredient: Call<RecipesByIngredientDTO?>?
+
+    @get:GET("lookup.php?i={id}")
+    val recipeById: Call<RecipesByIdDTO?>?
+
 }
