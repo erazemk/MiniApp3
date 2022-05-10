8fb61a6ed3816ccee931922f46a85cb497f00d63
Erazem Kokot <mail@erazem.eu>
2022-05-10 18:30:03 +0200

    Implement basic db operations for RecipeDao

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp3/database/dao/RecipeDao.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp3/database/dao/RecipeDao.kt
@@ -1,15 +1,24 @@
 package si.uni_lj.fri.pbd.miniapp3.database.dao
 
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 import si.uni_lj.fri.pbd.miniapp3.database.entity.RecipeDetails
 
 @Dao
 interface RecipeDao {
 
+    @Insert
+    fun insertRecipe(recipe: RecipeDetails)
+
     @Query("SELECT * FROM RecipeDetails WHERE idDrink = :idDrink")
     fun getRecipeById(idDrink: String?): RecipeDetails?
 
-    // TODO: Add the missing methods
+    @Query("DELETE FROM RecipeDetails WHERE idDrink = :idDrink")
+    fun deleteRecipe(idDrink: String?)
+
+    @get:Query("SELECT * FROM RecipeDetails")
+    val allRecipes: LiveData<List<RecipeDetails>>
 
 }
