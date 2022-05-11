2737d6473690dd884d788cf348f6c95d89368b21
Erazem Kokot <mail@erazem.eu>
2022-05-11 15:24:25 +0200

    Update SectionsPagerAdapter.kt

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp3/adapter/SectionsPagerAdapter.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp3/adapter/SectionsPagerAdapter.kt
@@ -1,11 +1,14 @@
 package si.uni_lj.fri.pbd.miniapp3.adapter
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import si.uni_lj.fri.pbd.miniapp3.ui.favorites.FavoritesFragment
 import si.uni_lj.fri.pbd.miniapp3.ui.search.SearchFragment
 
-class SectionsPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+class SectionsPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
 
     override fun getItemCount(): Int = 2
 
