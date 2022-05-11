cd5071286b97cdb821b8b6134f6ebf7e93145e6b
Erazem Kokot <mail@erazem.eu>
2022-05-11 15:26:24 +0200

    Clean up and fix ServiceGenerator.kt

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp3/rest/ServiceGenerator.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp3/rest/ServiceGenerator.kt
@@ -1,35 +1,31 @@
 package si.uni_lj.fri.pbd.miniapp3.rest
 
-import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
-import okhttp3.Protocol
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import si.uni_lj.fri.pbd.miniapp3.Constants
 import timber.log.Timber
-import java.util.*
 
 object ServiceGenerator {
+
     private var sBuilder: Retrofit.Builder? = null
     private var sHttpClient: OkHttpClient.Builder? = null
     private var sRetrofit: Retrofit? = null
-    private fun init() {
+
+    init {
         sHttpClient = OkHttpClient.Builder()
         sBuilder = Retrofit.Builder().baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create()) // TODO: add converter
+            .addConverterFactory(GsonConverterFactory.create())
 
-        /// TODO: create Interceptor and add it to client, what does this do?
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
 
-        sRetrofit = sBuilder?.client(sHttpClient?.build())?.build()
+        sRetrofit = sBuilder?.client(sHttpClient?.addInterceptor(interceptor)?.build()!!)?.build()
         Timber.d("Retrofit built with base url: %s", sRetrofit?.baseUrl()?.toUrl().toString())
     }
 
-    fun <S> createService(serviceClass: Class<S>?): S {
+    fun <S> createService(serviceClass: Class<S>): S {
         return sRetrofit!!.create(serviceClass)
-    }
-
-    init {
-        init()
     }
 }
