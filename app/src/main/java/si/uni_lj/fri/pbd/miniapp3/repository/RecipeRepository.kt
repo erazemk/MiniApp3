523d389f9b750932c13044fb8e54e228e009106d
Erazem Kokot <mail@erazem.eu>
2022-05-13 16:18:56 +0200

    Update RecipeRepository.kt

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp3/repository/RecipeRepository.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp3/repository/RecipeRepository.kt
@@ -10,6 +10,7 @@ import si.uni_lj.fri.pbd.miniapp3.database.Database
 import si.uni_lj.fri.pbd.miniapp3.database.dao.RecipeDao
 import si.uni_lj.fri.pbd.miniapp3.database.entity.RecipeDetails
 import si.uni_lj.fri.pbd.miniapp3.models.dto.IngredientsDTO
+import si.uni_lj.fri.pbd.miniapp3.models.dto.RecipeDetailsDTO
 import si.uni_lj.fri.pbd.miniapp3.models.dto.RecipesByIngredientDTO
 import si.uni_lj.fri.pbd.miniapp3.rest.RestAPI
 import si.uni_lj.fri.pbd.miniapp3.rest.ServiceGenerator
@@ -17,11 +18,14 @@ import timber.log.Timber
 
 class RecipeRepository(application: Application) {
 
-    val searchResults = MutableLiveData<List<RecipeDetails>>()
-    val recipeById = MutableLiveData<RecipeDetails>()
-    //val favorites = LiveData<List<RecipeDetailsIM>>?
+    val searchResults = MutableLiveData<RecipesByIngredientDTO>()
+    val searchRecipeById = MutableLiveData<RecipeDetails>()
+    val favorites = MutableLiveData<List<RecipeDetailsDTO>>()
+    val allIngredients = MutableLiveData<IngredientsDTO>()
     val allRecipes: LiveData<List<RecipeDetails>>?
+
     private val recipeDao: RecipeDao?
+    private val apiService = ServiceGenerator.createService(RestAPI::class.java)
 
     init {
         val db: Database? = application.let {
@@ -46,43 +50,12 @@ class RecipeRepository(application: Application) {
 
     fun findRecipe(recipeId: String) {
         Database.databaseWriteExecutor.execute {
-            recipeById.postValue(recipeDao?.findRecipe(recipeId))
+            searchRecipeById.postValue(recipeDao?.findRecipe(recipeId))
         }
     }
 
-    fun getIngredients(): IngredientsDTO {
-        val api = ServiceGenerator.createService(RestAPI::class.java)
-        val call = api.allIngredients
-        var allIngredients: IngredientsDTO
-
-        call.enqueue(object: Callback<IngredientsDTO?> {
-            override fun onResponse(
-                call: Call<IngredientsDTO?>,
-                response: Response<IngredientsDTO?>
-            ) {
-                if (response.isSuccessful) {
-                    val jsonData = response.body()
-                    Timber.d("Json: %s", jsonData)
-                    //val root = JSONObject(jsonData)
-
-                    //allIngredients = response.body() as IngredientsDTO
-                } else {
-                    Timber.d("Unsuccessful response to get all ingredients")
-                }
-            }
-
-            override fun onFailure(call: Call<IngredientsDTO?>, t: Throwable) {
-                Timber.d("Failed response to get all ingredients")
-            }
-        })
-
-        return IngredientsDTO()
-    }
-
-    fun getRecipesByIngredient(ingredient: String?): RecipesByIngredientDTO {
-        val api = ServiceGenerator.createService(RestAPI::class.java)
-        val call = api.getByIngredient(ingredient)
-        val recipes: RecipesByIngredientDTO
+    fun findRecipesByIngredient(ingredient: String?) {
+        val call = apiService.getRecipesByIngredient(ingredient)
 
         call?.enqueue(object: Callback<RecipesByIngredientDTO?> {
             override fun onResponse(
@@ -90,8 +63,7 @@ class RecipeRepository(application: Application) {
                 response: Response<RecipesByIngredientDTO?>
             ) {
                 if (response.isSuccessful) {
-                    val jsonData = response.body()
-                    Timber.d("Json: %s", jsonData)
+                    searchResults.postValue(response.body())
                 } else {
                     Timber.d("Unsuccessful response to get drinks by ingredient")
                 }
@@ -101,8 +73,28 @@ class RecipeRepository(application: Application) {
                 Timber.d("Failed response to get drinks by ingredient")
             }
         })
+    }
 
-        return RecipesByIngredientDTO()
+    fun getIngredients() {
+        val call = apiService.allIngredients
+
+        call.enqueue(object: Callback<IngredientsDTO?> {
+            override fun onResponse(
+                call: Call<IngredientsDTO?>,
+                response: Response<IngredientsDTO?>
+            ) {
+                if (response.isSuccessful) {
+                    allIngredients.postValue(response.body())
+                    Timber.d("Posted value to allIngredients")
+                } else {
+                    Timber.d("Unsuccessful response to get all ingredients")
+                }
+            }
+
+            override fun onFailure(call: Call<IngredientsDTO?>, t: Throwable) {
+                Timber.d("Failed response to get all ingredients")
+            }
+        })
     }
 
 }
