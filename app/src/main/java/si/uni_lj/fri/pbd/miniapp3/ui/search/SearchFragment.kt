ae1429c3c4e06d7a17ca7be15306a2d466965b22
Erazem Kokot <mail@erazem.eu>
2022-05-13 16:19:28 +0200

    Implement RecyclerView and Spinner in SearchFragment.kt

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp3/ui/search/SearchFragment.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp3/ui/search/SearchFragment.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import si.uni_lj.fri.pbd.miniapp3.R
 import si.uni_lj.fri.pbd.miniapp3.adapter.RecyclerViewAdapter
 import si.uni_lj.fri.pbd.miniapp3.adapter.SpinnerAdapter
-import si.uni_lj.fri.pbd.miniapp3.database.entity.RecipeDetails
 import si.uni_lj.fri.pbd.miniapp3.databinding.FragmentSearchBinding
 import si.uni_lj.fri.pbd.miniapp3.models.SearchViewModel
+import si.uni_lj.fri.pbd.miniapp3.models.dto.IngredientsDTO
+import timber.log.Timber
 
 class SearchFragment: Fragment(R.layout.fragment_search) {
 
@@ -21,8 +26,6 @@ class SearchFragment: Fragment(R.layout.fragment_search) {
     private var sViewModel: SearchViewModel? = null
     private var spinnerAdapter: SpinnerAdapter? = null
     private var recyclerAdapter: RecyclerViewAdapter? = null
-
-    private var recipes: List<RecipeDetails>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,10 +42,56 @@ class SearchFragment: Fragment(R.layout.fragment_search) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         sViewModel = ViewModelProvider(this)[SearchViewModel::class.java]
-        spinnerAdapter = SpinnerAdapter(sViewModel?.getIngredients())
-        // = RecyclerViewAdapter()
+        sViewModel?.getIngredients()
+        observerSetup()
+        recyclerSetup()
+    }
+
+    private fun observerSetup() {
+        // Observer for ingredients - spinner
+        sViewModel?.allIngredients?.observe(viewLifecycleOwner) { ingredients ->
+            Timber.d("Updated list of ingredients")
+            spinnerSetup(ingredients)
+        }
+
+        // Observer for drinks - recyclerView
+        sViewModel?.searchResults?.observe(viewLifecycleOwner) { recipes ->
+            Timber.d("Updated list of drinks")
+            recyclerAdapter?.setRecipes(recipes.drinks)
+        }
+    }
+
+    private fun recyclerSetup() {
+        recyclerAdapter = RecyclerViewAdapter()
+
+        val recyclerView: RecyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = recyclerAdapter
+    }
+
+    // Source: https://www.geeksforgeeks.org/spinner-in-kotlin/
+    private fun spinnerSetup(ingredients: IngredientsDTO) {
+        spinnerAdapter = SpinnerAdapter(ingredients)
+
+        val spinner: Spinner = binding.spinner
+        spinner.adapter = spinnerAdapter
+
+        // Testing before spinner works
+        sViewModel?.getRecipesByIngredient("Light rum")
+
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                sViewModel?.getRecipesByIngredient(spinnerAdapter?.getItem(position) as String)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
     }
 
 }
