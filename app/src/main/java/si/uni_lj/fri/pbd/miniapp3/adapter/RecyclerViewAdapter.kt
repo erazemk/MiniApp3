85123342ebbe0737fc2b192e607c7e40aca9654b
Erazem Kokot <mail@erazem.eu>
2022-05-13 17:07:58 +0200

    Fix downloading images in RecyclerViewAdapter.kt

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp3/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp3/adapter/RecyclerViewAdapter.kt
@@ -1,38 +1,32 @@
 package si.uni_lj.fri.pbd.miniapp3.adapter
 
 import android.content.Intent
-import android.content.res.Resources
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import si.uni_lj.fri.pbd.miniapp3.R
 import si.uni_lj.fri.pbd.miniapp3.models.dto.RecipeSummaryDTO
 import si.uni_lj.fri.pbd.miniapp3.ui.DetailsActivity
-import timber.log.Timber
-import java.io.IOException
+import java.io.InputStream
 import java.net.URL
 
-class RecyclerViewAdapter :
-    RecyclerView.Adapter<RecyclerViewAdapter.CardViewHolder>() {
+class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
-    inner class CardViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
-        var recipeId: String? = null
-        var recipeName: TextView? = null
-        var recipeThumbnail: ImageView? = null
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var recipeName: TextView
+        var recipeThumbnail: ImageView
 
         init {
-            recipeName = itemView?.findViewById(R.id.recipeTextView)
-            recipeThumbnail = itemView?.findViewById(R.id.recipeImageView)
+            recipeName = itemView.findViewById(R.id.recipeTextView)
+            recipeThumbnail = itemView.findViewById(R.id.recipeImageView)
         }
     }
 
@@ -43,21 +37,29 @@ class RecyclerViewAdapter :
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
-        return CardViewHolder(
+    override fun getItemCount(): Int {
+        return recipes?.size ?: 0
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.layout_grid_item, parent, false)
         )
     }
 
-    override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
-        val bitmap: Bitmap? = getBitmapFromUrl(recipes?.get(position)?.strDrinkThumb!!)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val url = recipes!![position].strDrinkThumb
+        val deferredBitmap: Deferred<Bitmap> = CoroutineScope(Dispatchers.IO).async { getThumbnail(url) }
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val bitmap: Bitmap = deferredBitmap.await()
+            bitmap.apply {
+                holder.recipeThumbnail.setImageBitmap(bitmap)
+            }
+        }
 
         holder.apply {
-            recipeId = recipes?.get(position)?.idDrink
-            recipeName?.text = recipes?.get(position)?.strDrink
-            recipeThumbnail?.setImageBitmap(bitmap)
-
-            // Open DetailsActivity and pass the clicked drink's ID to it
+            recipeName.text = recipes?.get(position)?.strDrink
             itemView.setOnClickListener {
                 val activity = it.context as? AppCompatActivity
                 val intent = Intent(activity, DetailsActivity::class.java)
@@ -67,30 +69,10 @@ class RecyclerViewAdapter :
         }
     }
 
-    override fun getItemCount(): Int {
-        recipes ?: return 0
-        return recipes!!.size
-    }
-
-    private fun getBitmapFromUrl(url: String): Bitmap? {
-        var recipeThumbnail: Bitmap? = null
-
-        CoroutineScope(Job() + Dispatchers.IO).launch {
-            recipeThumbnail = try {
-                val connection = URL(url)
-                val bitmap =
-                    BitmapFactory.decodeStream(connection.openConnection().getInputStream())
-                Bitmap.createScaledBitmap(bitmap, 100, 100, true)
-            } catch (ioe: IOException) {
-                Timber.d("Failed to download cocktail image")
-                BitmapFactory.decodeResource(
-                    Resources.getSystem(),
-                    android.R.drawable.picture_frame
-                )
-            }
-        }
-
-        return recipeThumbnail
+    private fun getThumbnail(url: String?) : Bitmap {
+        // Source: https://stackoverflow.com/questions/6407324/how-to-display-image-from-url-on-android
+        val inputStream: InputStream = URL(url).content as InputStream
+        return Drawable.createFromStream(inputStream, "thumbnail").toBitmap()
     }
 
 }
