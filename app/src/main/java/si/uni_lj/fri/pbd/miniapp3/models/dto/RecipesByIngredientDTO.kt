a428b703acd7df34e873f7df8061346490803e60
Erazem Kokot <mail@erazem.eu>
2022-05-13 12:39:48 +0200

    Add a RecipesByIngredientDTO.kt

---
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp3/models/dto/RecipesByIngredientDTO.kt
@@ -0,0 +1,12 @@
+package si.uni_lj.fri.pbd.miniapp3.models.dto
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class RecipesByIngredientDTO {
+
+    @SerializedName("drinks")
+    @Expose
+    val drinks: List<RecipeSummaryDTO>? = null
+
+}
