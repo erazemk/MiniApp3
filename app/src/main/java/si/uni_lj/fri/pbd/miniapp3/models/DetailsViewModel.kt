0ad993657533059493519b86260209ce5fc4af69
Erazem Kokot <mail@erazem.eu>
2022-05-13 20:35:00 +0200

    Implement details

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp3/models/DetailsViewModel.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp3/models/DetailsViewModel.kt
@@ -1,4 +1,41 @@
 package si.uni_lj.fri.pbd.miniapp3.models
 
-class DetailsViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import si.uni_lj.fri.pbd.miniapp3.database.entity.RecipeDetails
+import si.uni_lj.fri.pbd.miniapp3.repository.RecipeRepository
+import timber.log.Timber
+
+class DetailsViewModel(application: Application) : AndroidViewModel(application) {
+
+    val recipe: MutableLiveData<RecipeDetails>?
+    val favorites: LiveData<List<RecipeDetails>>?
+    private val repository: RecipeRepository = RecipeRepository(application)
+
+    init {
+        recipe = repository.searchRecipe
+        favorites = repository.allRecipes
+    }
+
+    fun getRecipe(recipeId: String, caller: String) {
+        Timber.d("Called DetailsViewModel.getRecipe(%s, %s)", recipeId, caller)
+        if (caller == "SearchFragment") {
+            // Download recipe
+            repository.findRecipeById(recipeId)
+        } else if (caller == "FavoritesFragment") {
+            // Get recipe from database
+            repository.findRecipe(recipeId)
+        }
+    }
+
+    fun addRecipe(recipe: RecipeDetails) {
+        repository.insertRecipe(recipe)
+    }
+
+    fun removeRecipe(recipe: RecipeDetails) {
+        repository.deleteRecipe(recipe.idDrink!!)
+    }
+
 }
--- app/src/main/java/si/uni_lj/fri/pbd/miniapp3/ui/DetailsActivity.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp3/ui/DetailsActivity.kt
@@ -1,4 +1,148 @@
 package si.uni_lj.fri.pbd.miniapp3.ui
 
-class DetailsActivity {
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.*
+import si.uni_lj.fri.pbd.miniapp3.R
+import si.uni_lj.fri.pbd.miniapp3.database.entity.RecipeDetails
+import si.uni_lj.fri.pbd.miniapp3.databinding.ActivityDetailsBinding
+import si.uni_lj.fri.pbd.miniapp3.models.DetailsViewModel
+import timber.log.Timber
+import java.io.InputStream
+import java.net.URL
+
+class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailsBinding
+
+    private var dViewModel: DetailsViewModel? = null
+    private var imageView: ImageView? = null
+    private var recipeName: TextView? = null
+    private var recipeIngredients: TextView? = null
+    private var recipeMeasures: TextView? = null
+    private var recipeInstructions: TextView? = null
+    private var favoritesButton: Button? = null
+
+    private var recipe: RecipeDetails? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        imageView = binding.detailsImage
+        recipeName = binding.detailsRecipeName
+        recipeIngredients = binding.detailsIngredients
+        recipeMeasures = binding.detailsMeasures
+        recipeInstructions = binding.detailsInstructions
+        favoritesButton = binding.detailsButton
+        favoritesButton?.text = getString(R.string.favorite)
+
+        val recipeId = intent.getStringExtra("recipeId") as String
+        val caller = intent.getStringExtra("caller") as String
+        Timber.d("Intent: recipeId=%s, caller=%s", recipeId, caller)
+
+        dViewModel = ViewModelProvider(this)[DetailsViewModel::class.java]
+        dViewModel?.getRecipe(recipeId, caller)
+
+        observerSetup()
+
+        favoritesButton?.setOnClickListener {
+            Timber.d("Clicked on favorites button, state=%b", recipe?.favorite)
+            if (recipe?.favorite == false) { // Add to database
+                recipe?.favorite = true
+                dViewModel?.addRecipe(recipe!!)
+                favoritesButton?.text = getString(R.string.unfavorite)
+            } else { // Remove from database
+                recipe?.favorite = false
+                dViewModel?.removeRecipe(recipe!!)
+                favoritesButton?.text = getString(R.string.favorite)
+            }
+        }
+    }
+
+    private fun observerSetup() {
+        dViewModel?.recipe?.observe(this) { recipe ->
+            this.recipe = recipe
+            setLayout()
+        }
+    }
+
+    private fun setLayout() {
+        // Set favorites button based on state
+        if (recipe?.favorite == true) {
+            favoritesButton?.text = getString(R.string.unfavorite)
+        } else {
+            favoritesButton?.text = getString(R.string.favorite)
+        }
+
+        val ingredients = mutableListOf(
+            recipe!!.strIngredient1,
+            recipe!!.strIngredient2,
+            recipe!!.strIngredient3,
+            recipe!!.strIngredient5,
+            recipe!!.strIngredient4,
+            recipe!!.strIngredient6,
+            recipe!!.strIngredient7,
+            recipe!!.strIngredient8,
+            recipe!!.strIngredient9,
+            recipe!!.strIngredient10,
+            recipe!!.strIngredient11,
+            recipe!!.strIngredient12,
+            recipe!!.strIngredient13,
+            recipe!!.strIngredient14,
+            recipe!!.strIngredient15
+        )
+
+        val measures = mutableListOf(
+            recipe!!.strMeasure1,
+            recipe!!.strMeasure2,
+            recipe!!.strMeasure3,
+            recipe!!.strMeasure5,
+            recipe!!.strMeasure4,
+            recipe!!.strMeasure6,
+            recipe!!.strMeasure7,
+            recipe!!.strMeasure8,
+            recipe!!.strMeasure9,
+            recipe!!.strMeasure10,
+            recipe!!.strMeasure11,
+            recipe!!.strMeasure12,
+            recipe!!.strMeasure13,
+            recipe!!.strMeasure14,
+            recipe!!.strMeasure15
+        )
+
+        // Remove null elements
+        ingredients.removeAll(listOf(null))
+        measures.removeAll(listOf(null))
+
+        recipeName?.text = recipe!!.strDrink
+        recipeIngredients?.text = ingredients.joinToString(", ")
+        recipeMeasures?.text = measures.joinToString(", ")
+        recipeInstructions?.text = recipe!!.strInstructions
+
+        val deferredImage: Deferred<Bitmap> = CoroutineScope(Dispatchers.IO).async {
+            getImage(recipe!!.strDrinkThumb)
+        }
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val bitmap: Bitmap = deferredImage.await()
+            bitmap.apply { imageView?.setImageBitmap(bitmap) }
+        }
+    }
+
+    private fun getImage(url: String?) : Bitmap {
+        // Source: https://stackoverflow.com/questions/6407324/how-to-display-image-from-url-on-android
+        val inputStream: InputStream = URL(url).content as InputStream
+        return Drawable.createFromStream(inputStream, "image").toBitmap()
+    }
+
 }
