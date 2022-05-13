42f527326918aa3aec0b28b898f6e6c9d1d2ad42
Erazem Kokot <mail@erazem.eu>
2022-05-13 21:58:05 +0200

    Always display logs because why not

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp3/CoreApplication.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp3/CoreApplication.kt
@@ -1,7 +1,6 @@
 package si.uni_lj.fri.pbd.miniapp3
 
 import android.app.Application
-import androidx.databinding.library.BuildConfig
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 
@@ -9,12 +8,6 @@ class CoreApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        plantTimber()
-    }
-
-    private fun plantTimber() {
-        if (BuildConfig.DEBUG) {
-            Timber.plant(DebugTree())
-        }
+        Timber.plant(DebugTree())
     }
 }
